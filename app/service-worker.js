95cb6682825cad2c69212b56e4ef2cc8954291b4
Joya <urnjoya@gmail.com>
2026-03-29 15:44:29 +0530

    Update service worker cache version and files

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = "linkvault-cache-v1.0.0";
+const CACHE_NAME = "linkvault-cache-v1.1.0";
 const urlsToCache = [
     "/linkvault/",
     "/linkvault/index.html",
 
     "/linkvault/css/style.css",
     "/linkvault/css/dark.css",
-
-    "/linkvault/js/core.js",
-    "/linkvault/js/links.js",
-    "/linkvault/js/ui.js",
-    "/linkvault/js/organize.js",
-    "/linkvault/js/tools.js",
-    "/linkvault/js/backup.js",
-    "/linkvault/js/analytics.js",
-    "/linkvault/js/pwa.js",
     "/linkvault/js/app.js"
 ];
 
